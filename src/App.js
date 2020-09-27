605e85372d13d25a0f7f1ee610b36e0c6b1a0b9a
Jahanzaiib <jahanzaibgcuf@gmail.com>
2020-09-27 22:58:01 +0500

    Add a new line to see the differece

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h2>Jahanzaib</h2>
-      <h3>Jahanzaib</h3>
-      <p>Jahanzaib</p>
-      <p>i am new in this city so please welcome me </p>
+        <h1>Hi all i add a new file</h1>
     </div>
   );
 }
