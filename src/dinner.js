40d5eae83f4b50890fd31ebaa3520d5ee848ca27
Jahanzaiib <jahanzaibgcuf@gmail.com>
2020-11-08 08:50:19 +0500

    new things or added

--- src/dinner.js
+++ src/dinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Dinner (props){
     return(
         <div>
-            <h1>Today we offer you 50% discount on {props.dishname}</h1>
+            <h1>Today we offer you 50% discount on on everything {props.dishname}</h1>
             <h1> For Food lover {props.sweet}</h1>
         </div>
     )
